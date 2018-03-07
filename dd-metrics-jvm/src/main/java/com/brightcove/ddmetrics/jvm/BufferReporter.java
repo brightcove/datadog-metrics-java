ee692a75c6c594ae0c9b077928ae65ea7fba43bc
Matt Whipple <mwhipple@brightcove.com>
2018-03-07 10:54:36 -0500

    Add jvm.buffers.direct.capacity

--- dd-metrics-jvm/src/main/java/com/brightcove/ddmetrics/jvm/BufferReporter.java
+++ dd-metrics-jvm/src/main/java/com/brightcove/ddmetrics/jvm/BufferReporter.java
@@ -31,23 +31,25 @@ public class BufferReporter implements Reporter {
     @EnsuresNonNull({"#1"})
     BufferReporter(List<BufferPoolMXBean> bufferBeans) {
 	if (bufferBeans == null)
-	    throw new NullPointerException("bufferBeans must not be null");
+	    throw new NullPointerException("bufferBeans must not be null.");
 	BufferPoolMXBean directPool = null;
 	for (BufferPoolMXBean bufferBean : bufferBeans) {
 	    if (bufferBean == null)
-		throw new NullPointerException("bufferBeans must not have nulls");
+		throw new NullPointerException("bufferBeans must not have nulls.");
 	    if (bufferBean.getName() == "direct") {
 		directPool = bufferBean;
 		// could break...but don't to complete null checking
 	    }
 	}
 	if (directPool == null)
-	    throw new IllegalArgumentException("direct pool must be present");
+	    throw new IllegalArgumentException("direct pool must be present.");
 	this.directPool = directPool;
     }
 
     public void report(StatsDClient client) {
 	client.recordGaugeValue("jvm.buffers.direct.count",
 				directPool.getCount());
+	client.recordGaugeValue("jvm.buffers.direct.capacity",
+				directPool.getTotalCapacity());
     }
 }
--- dd-metrics-jvm/src/test/groovy/com/brightcove/ddmetrics/jvm/BufferReporterSpecification.groovy
+++ dd-metrics-jvm/src/test/groovy/com/brightcove/ddmetrics/jvm/BufferReporterSpecification.groovy
@@ -1,10 +1,17 @@
 package com.brightcove.ddmetrics.jvm
 
 import java.lang.management.BufferPoolMXBean
+import javax.management.ObjectName
 
 import spock.lang.Specification
 
+import com.timgroup.statsd.StatsDClient
+
 class BufferReporterSpecification extends Specification {
+
+  StubBufferPoolMXBean directMBean = new StubBufferPoolMXBean(name: 'direct', count: 23L, totalCapacity: 1024L)
+  BufferReporter reporter = new BufferReporter([directMBean])  
+  StatsDClient client = Mock(StatsDClient)
 
   def 'Throws NullPointerException if bufferBeans is null.'() {
     when:
@@ -13,4 +20,61 @@ class BufferReporterSpecification extends Specification {
     then:
     thrown(NullPointerException)
   }
+
+  def 'Throws NullPointerException if bufferBeans contains any null values.'() {
+    when:
+    new BufferReporter(bufferBeans)
+
+    then:
+    thrown(NullPointerException)
+
+    where:
+    bufferBeans << [
+      [null],
+      [new StubBufferPoolMXBean(name: 'direct'), null],
+      [null, new StubBufferPoolMXBean(name: 'direct')]
+    ]
+  }
+
+  def 'Throws IllegalArgumentException if bufferBeans is empty.'() {
+    when:
+    new BufferReporter([])
+
+    then:
+    thrown(IllegalArgumentException)
+  }
+
+  def 'Throws IllegalArgumentException if bufferBeans does not contain direct pool.'() {
+    when:
+    new BufferReporter([new StubBufferPoolMXBean(name: 'indirect'),
+    	                new StubBufferPoolMXBean(name: 'transitive')])
+
+    then:
+    thrown(IllegalArgumentException)
+  }
+
+  def 'Reports count of direct buffers as jvm.buffers.direct.count.'() {
+    when:
+    reporter.report(client)
+
+    then:
+    1 * client.recordGaugeValue('jvm.buffers.direct.count', 23L)
+  }
+
+  def 'Reports count of direct buffers as jvm.buffers.direct.count.'() {
+    when:
+    reporter.report(client)
+
+    then:
+    1 * client.recordGaugeValue('jvm.buffers.direct.capacity', 1024L)
+  }
+
+}
+
+class StubBufferPoolMXBean implements BufferPoolMXBean {
+      long count
+      long memoryUsed
+      String name
+      long totalCapacity
+      ObjectName objectName
 }
